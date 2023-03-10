3f4db383b1f5cefcc46f9eb3c8e351c453bac32a
Kristina D <kristena91@mail.ru>
2023-03-10 14:47:34 +0300

    add src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticketPrice =13676;
+        int oneMilePrice =20;
+        int bonus =0;
+        if (ticketPrice >0) {bonus= ticketPrice /oneMilePrice;}
+        System.out.println("Количество бонусных миль за покупку: " + bonus
+        );
     }
 }
