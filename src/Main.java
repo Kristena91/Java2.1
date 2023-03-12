524248e92bdc8e3d0d6752ef28814790e2b15509
Kristina D <kristena91@mail.ru>
2023-03-12 11:21:16 +0300

    change2 src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice =13676;
-        int oneMilePrice =20;
-        int bonus =0;
-        if (ticketPrice >0) {bonus= ticketPrice /oneMilePrice;}
+        int ticketPrice = 13676;
+        int oneMilePrice = 20;
+        int bonus = 0;
+        if (ticketPrice > 0) {
+            bonus = ticketPrice / oneMilePrice;
+        }
         System.out.println("Количество бонусных миль за покупку: " + bonus
         );
     }
